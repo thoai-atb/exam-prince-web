af1bb0c969034d8c06eabba4f4e8f876ab4660f5
Lý Bảo Thoại <lybaothoai@gmail.com>
2025-11-09 14:16:50 +0700

    Fix github flow correct path

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/exam-prince/', // Add base URL for GitHub Pages
+  base: '/exam-prince-web/', // Add base URL for GitHub Pages
 })
